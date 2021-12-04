9f343eaabaf35dda7227c4acdbfbc623152619a4
edgarmarsolla <edgarmarsolla123@gmail.com>
2021-12-03 22:49:36 -0300

    Tentando corrigir o erro do deploy heroku2

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ PORT = '5432'
 BANCO = 'crud_usuario'
 
 
-CONN = "postgres://wunojgfktdysqc:7d307d0ba2bc2429203f8b7f0acbcf20b25cbd60204d8d55e586ea7f1977ed90@ec2-54-204-128-96.compute-1.amazonaws.com:5432/ddgc2scn5q8d15"
+CONN = "postgresql://wunojgfktdysqc:7d307d0ba2bc2429203f8b7f0acbcf20b25cbd60204d8d55e586ea7f1977ed90@ec2-54-204-128-96.compute-1.amazonaws.com:5432/ddgc2scn5q8d15"
 
 db_engine = create_engine(CONN)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
